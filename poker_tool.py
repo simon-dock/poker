df52314d8181d180ad99bb5e6511d3bb99202372
simon-dock <93451011+simon-dock@users.noreply.github.com>
2022-06-01 13:51:33 +0900

    maked prototype

--- poker_tool.py
+++ poker_tool.py
@@ -1,3 +1,23 @@
-n = int(input())
 
-print(n)
+def main():
+
+    End_Flag = True
+
+    print("start Tool of Poker")
+
+    data = []
+
+    while(End_Flag):
+
+        entered_char = input()
+        print("The character entered is ",entered_char)
+        data.append(entered_char)
+        
+
+        if entered_char == "q":
+            End_Flag = False
+
+    print(data)
+
+if __name__ == '__main__':
+    main()
