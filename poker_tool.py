e917700c520425cd3acf3720947bbd61a9728501
simon-dock <93451011+simon-dock@users.noreply.github.com>
2022-06-01 13:59:34 +0900

    maked prototype

--- poker_tool.py
+++ poker_tool.py
@@ -5,17 +5,27 @@ def main():
 
     print("start Tool of Poker")
 
+    # make_entrant_list() 参加者のデータリストを作成
     data = []
+
+    # manage_poket()　ポーカーの管理をする
 
     while(End_Flag):
 
         entered_char = input()
         print("The character entered is ",entered_char)
         data.append(entered_char)
-        
+         
+        # make        
 
         if entered_char == "q":
             End_Flag = False
+
+    # calculate_result() 戦績を精算する
+
+    # display_result() 結果を表示する
+
+    # export_reult() 結果をテキストファイルに出力する
 
     print(data)
 
